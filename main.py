821432b092b058e724f73d86547f842c30ade17c
bshahar <48634205+bshahar@users.noreply.github.com>
2022-02-23 15:05:12 +0200

    grid view

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter import *
+
+from matplotlib import figure
+from matplotlib.pyplot import plot
+
 import ui
-import generator
+import gridView
 
 #root = Tk()
 #my_gui = ui.GUI(root)
 #root.mainloop()
-
-grid = generator.crate_3d_grid(4, 6, 8, 40)
-print(generator.movement_3d_grid(grid, 2))
